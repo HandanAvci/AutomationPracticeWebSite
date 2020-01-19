28e387ad4efe1c74f4ff905b687b21b46f4035b9
nefer <nefer@LAPTOP-1RMVF318.home>
2020-01-18 23:55:25 -0500

    UPDATE sorted products(low to high price)

--- AutomationPractice/src/main/java/com/automationpractice/AutomationPractice.java
+++ AutomationPractice/src/main/java/com/automationpractice/AutomationPractice.java
@@ -1,16 +1,14 @@
 package com.automationpractice;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
@@ -25,169 +23,154 @@ import com.report.ExtentReport;
 import com.util.HighLighter;
 import com.util.TakescreenShot;
 
-
-
 public class AutomationPractice extends ExtentReport {
-   private WebDriver driver;
-   private 	MasterPageFactory pf;
-	
-    @BeforeTest
+	private WebDriver driver;
+	private MasterPageFactory pf;
+
+	@BeforeTest
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\nefer\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
-	 ChromeOptions chromeOptions =new ChromeOptions();
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\nefer\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		ChromeOptions chromeOptions = new ChromeOptions();
 		driver = new ChromeDriver(chromeOptions);
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 	}
-	
-    @Test 
+
+	@Test
 	public void login() throws Throwable {
-	String title = driver.getTitle();
+		String title = driver.getTitle();
 
-	System.out.println("My Store");
-	  
-	ObjectMap obj = new ObjectMap();
-	driver.get(obj.getconfig("URL"));
+		System.out.println("My Store");
 
-	 pf = PageFactory.initElements(driver, MasterPageFactory.class);
-  
+		ObjectMap obj = new ObjectMap();
+		driver.get(obj.getconfig("URL"));
 
-	HighLighter.colour(driver, pf.getSignin());
+		pf = PageFactory.initElements(driver, MasterPageFactory.class);
 
-	pf.getSignin().click();
-	Thread.sleep(2000);
-	HighLighter.colour(driver, pf.getEmail());
-	pf.getEmail().sendKeys(obj.getconfig("Name"));
+		HighLighter.colour(driver, pf.getSignin());
 
-	HighLighter.colour(driver, pf.getPassword());
-	pf.getPassword().sendKeys(obj.getconfig("Password"));
+		pf.getSignin().click();
+		Thread.sleep(2000);
+		HighLighter.colour(driver, pf.getEmail());
+		pf.getEmail().sendKeys(obj.getconfig("Name"));
 
-	HighLighter.colour(driver, pf.getSubmit());
-	
-	
-	
-	Thread.sleep(2000);
-	pf.getSubmit().click();
+		HighLighter.colour(driver, pf.getPassword());
+		pf.getPassword().sendKeys(obj.getconfig("Password"));
 
-}
+		HighLighter.colour(driver, pf.getSubmit());
 
-@Test (dependsOnMethods="login")
-public void product() {
-	
-	pf.getDresses().click();
-	
-	List<String> allproduct= new ArrayList<>();
-	for(int i=0; i<pf.getProductRow().size();i++) {
-		allproduct.add(pf.getProductRow().get(i).getText());
+		Thread.sleep(2000);
+		pf.getSubmit().click();
+
 	}
-	System.out.println("All product from Automation Practice::"+allproduct);
-	Set<String> noDuplicateProduct= new HashSet<>(allproduct);
-	System.out.println("Without duplicate product from"
-			+ " Automation Practice::"+noDuplicateProduct);
-	List<String> countItems=new ArrayList<>();	
-	
-	for(String item:allproduct) {
+
+	@Test(dependsOnMethods = "login")
+	public void product() {
+
+		pf.getDresses().click();
 		
-	
-		countItems.add(item+" count::"+Collections.frequency(allproduct, item));
+
+		List<String> allproduct = new ArrayList<>();
+		for (int i = 0; i < pf.getProductRow().size(); i++) {
+			allproduct.add(pf.getProductRow().get(i).getText());
+
+		}
+		System.out.println("All product from Automation Practice:" + allproduct);
+		Set<String> noDuplicateProduct = new HashSet<>(allproduct);
+		System.out.println("Without duplicate product from" + " Automation Practice:" + noDuplicateProduct);
+		List<String> countItems = new ArrayList<>();
+
+		for (String item : allproduct) {
+
+			countItems.add(item + " count:" + Collections.frequency(allproduct, item));
+		}
+
+		Set<String> noDuplicateCount = new HashSet<>(countItems);
+		System.out.println("Count of items from Automation Practice:" + noDuplicateCount);
+
+		String[] allPrice = { "$28.98 ", "$26.00", "$50.99", "$30.50", "$16.40" };
+		
+		List<Double> allPriceList = new ArrayList<>();
+		for (String price : allPrice) {
+			String dollar = price.replace("$", "").replace(",", "");
+			System.out.println(dollar);
+			allPriceList.add(Double.parseDouble(dollar));
+			Collections.sort(allPriceList);
+
+		}
+
+		System.out.println("All price List - Lowest first: " + allPriceList);
+
+		System.out.println("Max price: " + Collections.max(allPriceList));
+		System.out.println("Min price: " + Collections.min(allPriceList));
+		Set<Double> noDuplicatePrice = new HashSet<>(allPriceList);
+		System.out.println("Without duplicate price:" + noDuplicatePrice);
+
+		pf.getSeconddresses().click();
+		pf.getPinkDress().click();
+		Select drpdown = new Select(driver.findElement(By.name("group_1")));
+		drpdown.selectByValue("2");
+
+		pf.getIconPlus1().click();
+		pf.getSelector().click();
+
+		HighLighter.colour(driver, pf.getSecondDressSubmit());
+		pf.getSecondDressSubmit().click();
 	}
-	
-	Set<String> noDuplicateCount= new HashSet<>(countItems);
-	System.out.println("Count of items from Automation Practice::"+noDuplicateCount);
-	
-	String [] allPrice= {"$28.98 ","$26.00","$50.99","$30.50","$16.40"};	
-	List<Double> allPriceList=new ArrayList<>();		
-	for(String price:allPrice) {
-		String dollar=price.replace("$", "").replace(",", "");
-		System.out.println(dollar);
-		allPriceList.add(Double.parseDouble(dollar));
-		
+
+	@Test(dependsOnMethods = "product")
+	public void addTocart() throws InterruptedException {
+		Thread.sleep(1000);
+
+		HighLighter.colour(driver, pf.getSecondDressCheckOut());
+
+		TakescreenShot.captureScreenShot(driver, "Total Shipping");
+		Thread.sleep(2000);
+
+		pf.getSecondDressCheckOut().click();
+		Thread.sleep(2000);
+
+		HighLighter.colour(driver, pf.getCheckOut2());
+		Thread.sleep(2000);
+
+		pf.getCheckOut2().click();
+
+		pf.getCheckOut3().click();
 	}
-	
-		System.out.println("All price::"+allPriceList);
-		
-		System.out.println("Max price::"+Collections.max(allPriceList));
-		System.out.println("Min price::"+Collections.min(allPriceList));
-		Set<Double> noDuplicatePrice= new HashSet<>(allPriceList);
-		System.out.println("Without duplicate price::"+noDuplicatePrice);
-	
-	
-	
-	
-	pf.getSeconddresses().click();
-	pf.getPinkDress().click();
-	Select drpdown = new Select(driver.findElement(By.name("group_1")));
-	drpdown.selectByValue("2");
-	
-	pf.getIconPlus1().click();
-	pf.getSelector().click();
-	
-	
-	HighLighter.colour(driver, pf.getSecondDressSubmit());
-	pf.getSecondDressSubmit().click();
-}
 
-@Test(dependsOnMethods="product")
-public void addTocart() throws InterruptedException {
-Thread.sleep(1000);
-
-	
-	HighLighter.colour(driver, pf.getSecondDressCheckOut());
-	
-	TakescreenShot.captureScreenShot(driver, "Total Shipping");
-	Thread.sleep(2000);
-
-
-	pf.getSecondDressCheckOut().click();
-	Thread.sleep(2000);
-  
-	  HighLighter.colour(driver, pf.getCheckOut2());
-	  Thread.sleep(2000);
-	  
-   pf.getCheckOut2().click();
-	
-	
-	pf.getCheckOut3().click();
-}
-@Test(dependsOnMethods= "addTocart")
+	@Test(dependsOnMethods = "addTocart")
 	public void termsOfService() throws InterruptedException {
-	HighLighter.colour(driver, pf.getTermsOfService());
-	pf.getTermsOfService();
-	Thread.sleep(2000);
-	pf.getTermsOfService().click();
-}
+		HighLighter.colour(driver, pf.getTermsOfService());
+		pf.getTermsOfService();
+		Thread.sleep(2000);
+		pf.getTermsOfService().click();
+	}
 
-@Test(dependsOnMethods= "termsOfService")
-public void checkOut() throws InterruptedException {
-	
-	
-	pf.getLastCheckOut().click();
-	Thread.sleep(2000);
-	
+	@Test(dependsOnMethods = "termsOfService")
+	public void checkOut() throws InterruptedException {
 
-	HighLighter.colour(driver, pf.getCheque());
-	Thread.sleep(2000);
-	pf.getCheque().click();
+		pf.getLastCheckOut().click();
+		Thread.sleep(2000);
 
-	HighLighter.colour(driver, pf.getConfirmOrder());
-	TakescreenShot.captureScreenShot(driver, "Total Price");
-	pf.getConfirmOrder().click();
-	pf.getSignOut().click();
+		HighLighter.colour(driver, pf.getCheque());
+		Thread.sleep(2000);
+		pf.getCheque().click();
 
+		HighLighter.colour(driver, pf.getConfirmOrder());
+		TakescreenShot.captureScreenShot(driver, "Total Price");
+		pf.getConfirmOrder().click();
+		pf.getSignOut().click();
 
-	TakescreenShot.captureScreenShot(driver, "Automation login page");
-
+		TakescreenShot.captureScreenShot(driver, "Automation login page");
 
 	}
 
+	@AfterTest
+	public void teardown() {
 
-
-
-		@AfterTest
-		public void teardown() {
-		
 		driver.quit();
 
 	}
